9166db6db23f10fd121398e47e30401149f98951
rachidbend <rachidbendaghor@gmail.com>
2025-04-05 21:39:40 +0000

    fixed a bug with mobile nav

--- src/code.js
+++ src/code.js
@@ -3,16 +3,21 @@ const navButton = document.querySelector('.mobile-nav');
 const headerEl = document.querySelector('.header');
 const bodyEl = document.querySelector('body');
 
+let scrollY = 0;
 navButton.addEventListener('click', () => {
+  scrollY = window.scrollY;
   // 2- check the state of the nav (open or close), by checking if the header has the active class
 
   if (headerEl.classList.contains('active')) {
     // 3- if the nav is open, close it by removing the active class from the header
     headerEl.classList.remove('active');
     bodyEl.classList.remove('body-lock');
+    bodyEl.style.top = '';
+    window.scrollTo(0, scrollY);
   } else {
     // 4- if the nav is closed, open it by adding the active class to the header
     headerEl.classList.add('active');
     bodyEl.classList.add('body-lock');
+    bodyEl.style.top = `-${scrollY}px`;
   }
 });
